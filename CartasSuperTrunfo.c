18e544aa8fea65dcebfbe705853ae77c287db1a9
Renato Aguiar <109884233+RenatoAguiar13@users.noreply.github.com>
2026-01-14 02:03:13 +0000

    desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,82 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado1, estado2, cod_carta1[3], cod_carta2[3], nome_cidade1[20], nome_cidade2[20];
+    int populacao1, populacao2,area1, area2, ponto_turisticos1, ponto_turisticos2;
+    float pib1, pib2;
 
   // Área para entrada de dados
 
-  // Área para exibição dos dados da cidade
+   //Carta1 
+      
+    // estado  
+    printf("\nDigite o Estado da primeira carta (escolha uma letra entre A e H):\n");  
+    scanf("%c", &estado1);  
+    // cod_carta  
+    printf("\nDigite o codigo da carta:\n");  
+    scanf("%s", &cod_carta1);  
+    // cidade
+    printf("\nDigite o nome da cidade:\n");  
+    scanf("%s", &nome_cidade1);  
+    // população 
+    printf("\nDigite a população da cidade:\n");  
+    scanf("%d", &populacao1);  
+    // área  
+    printf("\nDigite a área da cidade:\n");  
+    scanf("%f", &area1);  
+    // pib  
+    printf("\nDigite o PIB da cidade:\n");  
+    scanf("%f", &pib1);  
+    // pontos turisticos  
+    printf("\nDigite a quantidade de pontos turisticos que existem na cidade:\n");  
+    scanf("%d", &ponto_turisticos1);  
+  
+  //Carta2 
 
+    // estado  
+    printf("\nDigite o Estado da primeira carta (escolha uma letra entre A e H):\n");  
+    scanf("%c", &estado2);  
+    // cod_carta  
+    printf("\nDigite o codigo da carta:\n");  
+    scanf("%s", &cod_carta2);  
+    // cidade
+    printf("\nDigite o nome da cidade:\n");  
+    scanf("%s", &nome_cidade2);  
+    // população 
+    printf("\nDigite a população da cidade:\n");  
+    scanf("%d", &populacao2);  
+    // área  
+    printf("\nDigite a área da cidade:\n");  
+    scanf("%f", &area2);  
+    // pib  
+    printf("\nDigite o PIB da cidade:\n");  
+    scanf("%f", &pib2);  
+    // pontos turisticos  
+    printf("\nDigite a quantidade de pontos turisticos que existem na cidade:\n");  
+    scanf("%d", &ponto_turisticos2);  
+
+  // Área para exibição dos dados da cidade 
+  
+    printf("Carta 1: \n");  
+    printf("\nEstado: %c\n", estado1);  
+    printf("Codigo: %s\n", cod_carta1);  
+    printf("Nome da cidade: %s\n", nome_cidade1);  
+    printf("População: %d\n", populacao1);  
+    printf("Área: %f\n", area1);  
+    printf("PIB: %f\n", pib1);  
+    printf("Número de pontos Turísticos: %d\n", ponto_turisticos1);  
+
+  
+    printf("Carta 2: \n");  
+    printf("\nEstado: %c\n", estado2);  
+    printf("Codigo: %s\n", cod_carta2);  
+    printf("Nome da cidade: %s\n", nome_cidade2);  
+    printf("População: %d\n", populacao2);  
+    printf("Área: %f\n", area2);  
+    printf("PIB: %f\n", pib2);  
+    printf("Número de pontos Turísticos: %d\n", ponto_turisticos2);  
+      
+   
+    
 return 0;
 } 
